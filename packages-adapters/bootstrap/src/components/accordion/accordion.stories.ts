4592379dbb7930478019aeda2ce85aab462b74f6
Viacheslav Sen <v.sen@outlook.com>
2023-02-15 20:39:06 +0200

    [#112] WIP accordion alert

--- packages-adapters/bootstrap/src/components/accordion/accordion.stories.ts
+++ packages-adapters/bootstrap/src/components/accordion/accordion.stories.ts
@@ -9,9 +9,65 @@ export default meta;
 
 export const Default: Story = (args): StoryFnAureliaReturnType => ({
   components: [BsAccordionItem],
-  innerHtml: `
-    <bs-accordion-item header="Item 1" collapsed.bind="false">Lorem ipsum</bs-accordion-item>
-    <bs-accordion-item header="Item 2">Lorem ipsum 2</bs-accordion-item>
+  props: {
+    innerHtml: `
+    <bs-accordion-item header="Accordion Item #1" collapsed.bind="false">
+        <strong>This is the first item's accordion body.</strong>
+        It is shown by default.
+    </bs-accordion-item>
+    <bs-accordion-item header="Accordion Item #2">
+        <strong>This is the second item's accordion body.</strong>
+        It is hidden by default.
+    </bs-accordion-item>
+    <bs-accordion-item header="Accordion Item #3">
+        <strong>This is the third item's accordion body.</strong>
+        It is hidden by default
+    </bs-accordion-item>
+  `,
+    ...args,
+  },
+});
+
+export const FlushExample: Story = (args): StoryFnAureliaReturnType => ({
+  components: [BsAccordionItem],
+  template: `
+    <bs-accordion flush.bind="true">
+      <bs-accordion-item header="Accordion Item #1" collapsed.bind="false">
+          <strong>This is the first item's accordion body.</strong>
+          It is shown by default.
+      </bs-accordion-item>
+      <bs-accordion-item header="Accordion Item #2">
+          <strong>This is the second item's accordion body.</strong>
+          It is hidden by default.
+      </bs-accordion-item>
+      <bs-accordion-item header="Accordion Item #3">
+          <strong>This is the third item's accordion body.</strong>
+          It is hidden by default
+      </bs-accordion-item>
+    </bs-accordion>
+  `,
+  props: {
+    ...args,
+  },
+});
+
+export const AlwaysOpenExample: Story = (args): StoryFnAureliaReturnType => ({
+  components: [BsAccordionItem],
+  template: `
+    <bs-accordion always-open.bind="true">
+      <bs-accordion-item header="Accordion Item #1" collapsed.bind="false">
+          <strong>This is the first item's accordion body.</strong>
+          It is shown by default.
+      </bs-accordion-item>
+      <bs-accordion-item header="Accordion Item #2">
+          <strong>This is the second item's accordion body.</strong>
+          It is hidden by default.
+      </bs-accordion-item>
+      <bs-accordion-item header="Accordion Item #3">
+          <strong>This is the third item's accordion body.</strong>
+          It is hidden by default
+      </bs-accordion-item>
+    </bs-accordion>
   `,
   props: {
     ...args,
