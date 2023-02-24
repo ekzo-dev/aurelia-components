13cdfaeba95d3190cd568f2f9e7d68cd0397c5d3
Viacheslav Sen <v.sen@outlook.com>
2023-02-24 14:11:21 +0200

    [#112] WIP tooltip

--- packages-adapters/bootstrap/src/components/tooltip/tooltip.stories.ts
+++ packages-adapters/bootstrap/src/components/tooltip/tooltip.stories.ts
@@ -1,11 +1,16 @@
 import { Meta, Story, StoryFnAureliaReturnType } from '@storybook/aurelia';
 import { BsTooltip } from '.';
 
+import './tooltip.stories.scss';
+
 export default {
   title: 'Bootstrap / Components / Tooltip',
   args: {
     animation: false,
     placement: 'top',
+    title: 'Default tooltip',
+    delay: 0,
+    html: true,
   },
 } as Meta;
 
@@ -14,7 +19,7 @@ export const Default: Story = (args): StoryFnAureliaReturnType => ({
   template: `
     <div class="container w-75">
       <span>Some text without tooltip,</span><br>
-      <strong bs-tooltip="title: Default tooltip; animation.bind: animation; placement.bind: placement;">some text with tooltip.</strong>
+      <strong bs-tooltip="delay.bind: delay; title.bind: title; animation.bind: animation; placement.bind: placement;">some text with tooltip.</strong>
     <div class="container">
   `,
   props: {
@@ -22,16 +27,46 @@ export const Default: Story = (args): StoryFnAureliaReturnType => ({
   },
 });
 
-export const HtmlTooltipExample: Story = (args): StoryFnAureliaReturnType => ({
+export const DelayOneSecExample: Story = (args): StoryFnAureliaReturnType => ({
   components: [BsTooltip],
   template: `
     <div class="container w-75">
       <span>Some text without tooltip,</span><br>
-      <strong bs-tooltip="template.bind: template; title.bind: 'Allow <em><u>HTML</u> <strong>in the tooltip</strong></em>'; animation.bind: animation; placement.bind: placement; html.bind: true">some text with tooltip.</strong>
+      <strong bs-tooltip="delay.bind: delay; title.bind: title; animation.bind: animation; placement.bind: placement;">some text with tooltip.</strong>
     <div class="container">
   `,
   props: {
     ...args,
-    template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>',
+    delay: 1000,
+  },
+});
+
+export const HtmlDefaultTooltipExample: Story = (args): StoryFnAureliaReturnType => ({
+  components: [BsTooltip],
+  template: `
+    <div class="container text-center">
+      <span>Some text without tooltip,</span><br>
+      <strong bs-tooltip="delay.bind: delay; template.bind: template; title.bind: title; animation.bind: animation; placement.bind: placement; html.bind: html">some text with tooltip.</strong>
+    <div class="container">
+  `,
+  props: {
+    ...args,
+    title: 'Allow <em><u>HTML</u> <strong>in the</span> tooltip</strong></em>',
+  },
+});
+
+export const CustomTooltipExample: Story = (args): StoryFnAureliaReturnType => ({
+  components: [BsTooltip],
+  template: `
+   <div class="container text-center">
+          <span>Some text without tooltip,</span><br>
+          <strong bs-tooltip="delay.bind: delay; template.bind: template; title.bind: title; animation.bind: animation; placement.bind: placement; html.bind: html">some text with tooltip.</strong>
+    </div>
+  `,
+  props: {
+    ...args,
+    template:
+      '<div class="tooltip tooltip-example" style="background-color: darkred;" role="tooltip"><div class="tooltip-inner"></div></div>',
+    title: 'Allow <em><u>HTML</u> <strong>in the</span> tooltip</strong></em>',
   },
 });
