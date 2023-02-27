c04a1ceb4f0ca285026d10a7245b5ab9f67cb9dc
Viacheslav Sen <v.sen@outlook.com>
2023-02-27 19:33:05 +0200

    [#112] WIP badge (accordion, alert, badge, breadcrumb fix)

--- packages-adapters/bootstrap/src/components/badge/badge.stories.ts
+++ packages-adapters/bootstrap/src/components/badge/badge.stories.ts
@@ -16,8 +16,8 @@ const Default: Story = (args): StoryFnAureliaReturnType => ({
 const PositioningExample: Story = (args): StoryFnAureliaReturnType => ({
   components: [BsButton],
   template: `
-  <bs-button>INBOX</bs-button><bs-badge pill.bind="pill" variant.bind="variant" style="position: relative; top: -15px; left: -15px;">99+</bs-badge>
-  <bs-button class="me-4">Notifications <bs-badge  pill.bind="pill" variant.bind="variant">4</bs-badge></bs-button>
+<bs-button>INBOX</bs-button><bs-badge pill.bind="pill" variant.bind="variant" style="position: relative; top: -15px; left: -15px;">99+</bs-badge>
+<bs-button class="me-4">Notifications <bs-badge  pill.bind="pill" variant.bind="variant">4</bs-badge></bs-button>
   `,
 });
 PositioningExample.args = {
