f4f1062544ed248a4b3f6d810ef7985031b95618
Viacheslav Sen <v.sen@outlook.com>
2023-02-27 18:30:57 +0200

    [#112] WIP badge (story fix, component fix)

--- packages-adapters/bootstrap/src/components/badge/badge.stories.ts
+++ packages-adapters/bootstrap/src/components/badge/badge.stories.ts
@@ -8,48 +8,23 @@ const meta: Meta = {
 };
 export default meta;
 
-export const Default: Story = (args): StoryFnAureliaReturnType => ({
-  props: {
-    ...args,
-    innerHtml: 'NEW',
-  },
-});
-
-export const PillExample: Story = (args): StoryFnAureliaReturnType => ({
-  components: [BsBadge],
-  template: `
-  <bs-badge pill.bind="true">Pill</bs-badge>
-  `,
-  props: {
-    pill: true,
-    ...args,
-  },
-});
-
-export const ColorsExample: Story = (args): StoryFnAureliaReturnType => ({
-  components: [BsBadge],
-  template: `
-  <bs-badge>Primary</bs-badge>
-  <bs-badge variant="secondary" class="m-1">Secondary</bs-badge>
-  <bs-badge variant="success" class="m-1">Success</bs-badge>
-  <bs-badge variant="danger" class="m-1">Danger</bs-badge>
-  <bs-badge variant="warning" class="m-1">Warning</bs-badge>
-  <bs-badge variant="info" class="m-1">Info</bs-badge>
-  <bs-badge variant="light" class="m-1">Light</bs-badge>
-  <bs-badge variant="dark" class="m-1">Dark</bs-badge>
-  `,
+const Default: Story = (args): StoryFnAureliaReturnType => ({
+  innerHtml: 'NEW',
   props: {
     ...args,
   },
 });
 
-export const PositioningExample: Story = (args): StoryFnAureliaReturnType => ({
-  components: [BsBadge, BsButton],
+const PositioningExample: Story = (args): StoryFnAureliaReturnType => ({
+  components: [BsButton],
   template: `
-  <bs-button>INBOX</bs-button><bs-badge pill.bind="true" variant="danger" style="position: relative; top: -15px; left: -15px;">99+</bs-badge>
-  <bs-button class="me-4">Notifications <bs-badge variant="secondary">4</bs-badge></bs-button>
+  <bs-button>INBOX</bs-button><bs-badge pill.bind="pill" variant.bind="variant" style="position: relative; top: -15px; left: -15px;">99+</bs-badge>
+  <bs-button class="me-4">Notifications <bs-badge  pill.bind="pill" variant.bind="variant">4</bs-badge></bs-button>
   `,
-  props: {
-    ...args,
-  },
 });
+PositioningExample.args = {
+  variant: 'danger',
+  pill: false,
+};
+
+export { Default, PositioningExample };
