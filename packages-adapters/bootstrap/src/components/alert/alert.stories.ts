c7e72209c836f1827d30d8c66442c7d0a6554dc6
Viacheslav Sen <v.sen@outlook.com>
2023-03-02 23:58:16 +0200

    [#112] WIP (alert stories added createComponentTemplate)

--- packages-adapters/bootstrap/src/components/alert/alert.stories.ts
+++ packages-adapters/bootstrap/src/components/alert/alert.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, Story, StoryFnAureliaReturnType } from '@storybook/aurelia';
+import { Meta, Story, StoryFnAureliaReturnType, createComponentTemplate } from '@storybook/aurelia';
 import { BsAlert } from '.';
 import { BsIcon } from '../../icon';
 import { selectControl, defaultVariants } from '../../story';
@@ -25,14 +25,22 @@ const Default: Story = (args): StoryFnAureliaReturnType => ({
 const AdditionalContent: Story = (args): StoryFnAureliaReturnType => ({
   components: [BsIcon],
   template: `
-<bs-alert variant.bind="variant" dismissible.bind="dismissible">
+${createComponentTemplate(
+  BsAlert,
+  `
     <h4 class="alert-heading">Well done!</h4>
-    <p class="mb-0">A simple primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.<br>Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+    <p class="mb-0">A simple primary alert
+    with <a href="#" class="alert-link">an example link</a>.
+    Give it a click if you like.<br>Whenever you need to, be sure
+    to use margin utilities to keep things nice and tidy.</p>
     <hr>
-    <p>You successfully read this alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+    <p>You successfully read this alert message.
+    This example text is going to run a bit longer so that you can see
+    how spacing within an alert works with this kind of content.</p>
     <p class="mb-0"><bs-icon name="exclamation-triangle-fill" class="me-2"></bs-icon>An example alert with an icon.</p>
-</bs-alert>
-  `,
+`
+)}
+`,
   props: args,
 });
 AdditionalContent.args = {
