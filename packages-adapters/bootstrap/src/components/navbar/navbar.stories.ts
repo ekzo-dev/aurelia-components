a5b634f3293349d0861b612a59aee0f741f22962
Viacheslav Sen <v.sen@outlook.com>
2023-03-21 11:44:51 +0200

    [#112] WIP (navbar)

--- packages-adapters/bootstrap/src/components/navbar/navbar.stories.ts
+++ packages-adapters/bootstrap/src/components/navbar/navbar.stories.ts
@@ -2,10 +2,10 @@ import { Meta, Story, createComponentTemplate } from '@storybook/aurelia';
 import { BsNavbar, BsNavbarNav, BsNavbarBrand, BsNavbarText } from '.';
 import { BsNavItem, BsNavLink } from '../nav';
 import { BsDropdown, BsDropdownItem, BsDropdownMenu, BsDropdownToggle } from '../dropdown';
-
-import './navbar.scss';
-import '../nav/nav.scss';
-import '../dropdown/dropdown.scss';
+import { BsInput } from '../../forms';
+import { BsButton } from '../button';
+import { BsCollapse } from '../collapse';
+import { BsIcon } from '../../icon';
 
 import { selectControl, variantOptions } from '../../story';
 
@@ -25,43 +25,90 @@ const meta: Meta = {
 export default meta;
 
 const Default: Story = (args) => ({
-  components: [
-    BsNavbarBrand,
-    BsNavbarNav,
-    BsNavbarText,
-    BsNavItem,
-    BsNavLink,
-    BsDropdown,
-    BsDropdownMenu,
-    BsDropdownToggle,
-    BsDropdownItem,
-  ],
+  components: [BsNavbarBrand, BsNavbarNav, BsNavbarText, BsNavItem, BsNavLink],
   template: createComponentTemplate(
     BsNavbar,
     `
-<div class="container-fluid">
-${createComponentTemplate(BsNavbarBrand, '<a href="#" target="_self">Navbar</a>')}
-${createComponentTemplate(
-  BsNavbarNav,
-  `
-    <bs-nav-item>
-        <bs-nav-link active><a href="#" target="_self">Active</a></bs-nav-link>
-    </bs-nav-item>
-    <bs-nav-item>
-       <bs-nav-link><a href="#" target="_self">Much longer nav link</a></bs-nav-link>
-    </bs-nav-item>
-    <bs-nav-item>
-        <bs-nav-link><a href="#" target="_self">Link</a></bs-nav-link>
-    </bs-nav-item>
-    <bs-nav-item>
-        <bs-nav-link disabled><a href="#" target="_self">Link</a></bs-nav-link>
-    </bs-nav-item>`
-)}
-${createComponentTemplate(BsNavbarText, 'Navbar text with an inline element ')}
-</div>
+    <div class="container-fluid">
+      ${createComponentTemplate(
+        BsNavbarBrand,
+        `
+        <a href="#" target="_self">Navbar</a>
+      `
+      )}
+      ${createComponentTemplate(
+        BsNavbarNav,
+        `
+          <bs-nav-item>
+              <bs-nav-link active><a href="#" target="_self">Active</a></bs-nav-link>
+          </bs-nav-item>
+          <bs-nav-item>
+              <bs-nav-link><a href="#" target="_self">Much longer nav link</a></bs-nav-link>
+          </bs-nav-item>
+          <bs-nav-item>
+              <bs-nav-link><a href="#" target="_self">Link</a></bs-nav-link>
+          </bs-nav-item>
+          <bs-nav-item>
+              <bs-nav-link disabled><a href="#" target="_self">Link</a></bs-nav-link>
+          </bs-nav-item>
+      `
+      )}
+      ${createComponentTemplate(BsNavbarText, `Navbar text with an inline element`)}
+    </div>
 `
   ),
   props: args,
 });
 
-export { Default };
+const collapsedScrolled: Story = (args) => ({
+  components: [BsNavbarBrand, BsNavbarNav, BsNavbarText, BsNavItem, BsNavLink, BsButton, BsCollapse, BsIcon],
+  template: createComponentTemplate(
+    BsNavbar,
+    `
+    <div class="container-fluid">
+      ${createComponentTemplate(
+        BsNavbarBrand,
+        `
+        <a href="#" target="_self">Navbar</a>
+      `
+      )}
+      <bs-button variant.bind="variant" class="navbar-toggler" click.trigger="collapse.collapsedChanged()">
+          <bs-icon name="list">
+      </bs-button>
+      <bs-collapse class="navbar-collapse" horizontal.bind="horizontal" collapsed.bind="collapsed" view-model.ref="collapse">
+          ${createComponentTemplate(
+            BsNavbarNav,
+            `
+              <bs-nav-item>
+                  <bs-nav-link active><a href="#" target="_self">Active</a></bs-nav-link>
+              </bs-nav-item>
+              <bs-nav-item>
+                  <bs-nav-link><a href="#" target="_self">Much longer nav link</a></bs-nav-link>
+              </bs-nav-item>
+              <bs-nav-item>
+                  <bs-nav-link><a href="#" target="_self">Link</a></bs-nav-link>
+              </bs-nav-item>              <bs-nav-item>
+                  <bs-nav-link><a href="#" target="_self">Link</a></bs-nav-link>
+              </bs-nav-item>              <bs-nav-item>
+                  <bs-nav-link><a href="#" target="_self">Link</a></bs-nav-link>
+              </bs-nav-item>              <bs-nav-item>
+                  <bs-nav-link><a href="#" target="_self">Link</a></bs-nav-link>
+              </bs-nav-item>
+              <bs-nav-item>
+                  <bs-nav-link disabled><a href="#" target="_self">Link</a></bs-nav-link>
+              </bs-nav-item>
+          `
+          )}
+      </bs-collapse>
+    </div>
+`
+  ),
+  props: args,
+});
+collapsedScrolled.args = {
+  collapsed: false,
+  expand: 'xxl',
+  scroll: true,
+};
+
+export { Default, collapsedScrolled };
--- packages-adapters/bootstrap/src/components/navbar/navbar.ts
+++ packages-adapters/bootstrap/src/components/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { customElement, bindable, ICustomElementViewModel } from 'aurelia';
 import './navbar.scss';
 import template from './navbar.html';
 import { Variants } from '../../interfaces';
+import { coerceBoolean } from '../../utils';
 
 export type Expands = 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 
@@ -15,4 +16,15 @@ export class BsNavbar implements ICustomElementViewModel {
 
   @bindable()
   variant?: Variants;
+
+  @bindable(coerceBoolean)
+  darkBackground?: boolean = false;
+
+  /* todo
+  - navbar-toggler for use with our collapse plugin and other navigation toggling behaviors.
+  - Add an optional .navbar-scroll to set a max-height and scroll expanded navbar content.
+
+  For navbars that never collapse, add the .navbar-expand class on the navbar.
+  For navbars that always collapse, don’t add any .navbar-expand class.
+  */
 }
