c5a17a567d4b2d3c6808fa0e27265f3dc8bc0fa7
Архипов Дмитрий <da@netcitylife.ru>
2023-04-29 23:10:13 +0300

    fix(bs-select-multiple): fix size attribute

--- packages/bs-select-multiple/src/select-multiple/select-multiple.ts
+++ packages/bs-select-multiple/src/select-multiple/select-multiple.ts
@@ -4,6 +4,11 @@ import { BsCheckbox, BsSelect } from '@ekzo/bootstrap';
 import template from './select-multiple.html';
 import './select-multiple.scss';
 
+const BS_SIZE_MULTIPLIER = {
+  lg: 1.125,
+  sm: 0.875,
+};
+
 /**
  * TODO: support required native attribute
  */
@@ -16,35 +21,44 @@ export class BsSelectMultiple extends BsSelect {
   @bindable({ mode: BindingMode.twoWay })
   value!: any[];
 
+  control!: HTMLDivElement;
+
   constructor(private element: HTMLElement) {
     super();
   }
 
   attached() {
     this.setHeight(true);
+    this.scrollToSelected();
   }
 
-  sizeChanged() {
-    this.setHeight();
-  }
-
-  bsSizeChanged() {
-    this.setHeight();
+  propertyChanged(name: keyof this) {
+    switch (name) {
+      case 'size':
+      case 'bsSize':
+      case 'floatingLabel':
+        setTimeout(() => this.setHeight());
+    }
   }
 
   private setHeight(init = false): void {
-    const { style } = this.element;
-    if (style.height && init) return;
+    const { style } = this.control;
 
     if (this.size > 0) {
-      const { borderTopWidth, borderBottomWidth, paddingTop, paddingBottom } = getComputedStyle(
-        this.element.querySelector('div.form-control')
-      );
+      const { borderTopWidth, borderBottomWidth, paddingTop, paddingBottom } = getComputedStyle(this.control);
       style.height = `calc(${
-        this.size * 1.625
-      }rem + ${borderTopWidth} + ${borderBottomWidth} + ${paddingTop} + ${paddingBottom})`;
+        this.size * 1.625 * (this.bsSize ? BS_SIZE_MULTIPLIER[this.bsSize] : 1)
+      }rem + ${borderTopWidth} + ${borderBottomWidth} + ${paddingTop} + ${paddingBottom} - 2px)`;
     } else if (style.height) {
-      style.height = 'auto';
+      style.height = undefined;
+    }
+  }
+
+  private scrollToSelected() {
+    const selected = this.element.querySelector<HTMLInputElement>('input:checked');
+    if (selected) {
+      const { paddingTop } = getComputedStyle(this.control);
+      this.control.scrollTo({ top: selected.parentElement.offsetTop - parseInt(paddingTop) });
     }
   }
 }
