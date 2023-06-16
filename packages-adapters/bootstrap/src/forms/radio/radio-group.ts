6a6d077e1b492bdc9bbfd5314101c22344853ab8
Архипов Дмитрий <da@netcitylife.ru>
2023-06-17 00:41:50 +0200

    feat(bootstrap): add missing dep

--- packages-adapters/bootstrap/src/forms/radio/radio-group.ts
+++ packages-adapters/bootstrap/src/forms/radio/radio-group.ts
@@ -1,13 +1,14 @@
 import { bindable, BindingMode, customElement } from 'aurelia';
 import { coerceBoolean, uniqueId, Iterable } from '@ekzo-dev/toolkit';
 import { BaseField } from '../base-field';
+import { BsRadio } from './radio';
 import template from './radio-group.html';
 import './radio.scss';
 
 @customElement({
   name: 'bs-radio-group',
   template,
-  dependencies: [Iterable],
+  dependencies: [Iterable, BsRadio],
 })
 export class BsRadioGroup extends BaseField {
   @bindable({ mode: BindingMode.twoWay })
