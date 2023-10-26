59651e3a5ad7ff287ba1c1e6fe08d5502bc2804f
Sergey Shakhmatov <sergey_shakhmatov@epam.com>
2023-10-26 13:52:25 +0200

    refactor(config): configure ling-staged, update husky, add ncu to update deps

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,9 +1,22 @@
 'use strict';
 
+const {
+  utils: { getPackages },
+  // eslint-disable-next-line node/no-unpublished-require
+} = require('@commitlint/config-lerna-scopes');
+const {
+  utils: { getProjects },
+  // eslint-disable-next-line node/no-unpublished-require
+} = require('@commitlint/config-nx-scopes');
+
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes', '@commitlint/config-nx-scopes'],
   rules: {
     'header-max-length': [2, 'always', 100],
-    'scope-enum': [1, 'always', []],
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [...(await getPackages(ctx)), ...(await getProjects(ctx)), 'app', 'config', 'env', 'deps'],
+    ],
   },
 };
