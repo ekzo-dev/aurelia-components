c8d7d6f70d7775e28b28e62117c935da9b18c8db
Sergey Shakhmatov <sergey_shakhmatov@epam.com>
2023-10-26 12:54:56 +0200

    refactor(lint-staged): configure ling-staged, update husky, add ncu to update deps

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,6 +4,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes', '@commitlint/config-nx-scopes'],
   rules: {
     'header-max-length': [2, 'always', 100],
-    'scope-enum': [2, 'always', ['app', 'env', 'workflow']],
+    'scope-enum': [1, 'always', []],
   },
 };
